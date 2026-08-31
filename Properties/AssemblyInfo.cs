99f03e03be445683637a9cc7c0a3fb99996707df
Kristian Tsenov <55602145+ktsenovv@users.noreply.github.com>
2026-08-31 18:54:54 +0300

    Add AssemblyInfo.cs for Steam Server Browser

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Steam Server Browser")]
+[assembly: AssemblyDescription("Adds a Playnite top-panel button that opens Steam's native Game Server Browser.")]
+[assembly: AssemblyCompany("H4K70RX")]
+[assembly: AssemblyProduct("SteamServerBrowser")]
+[assembly: ComVisible(false)]
+[assembly: Guid("3C0C2C2E-1C7D-45F5-BA11-7D4C0A9D9D21")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
